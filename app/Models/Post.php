0fbf8820dc670d27b51512b453fa2151705be2c8
Mharks001 <ahmad.mhmd.sakr@gmail.com>
2023-05-12 11:05:06 +0300

    Added Guards on Post model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -9,10 +9,9 @@ class Post extends Model
 {
     use HasFactory;
 
-    protected $fillable = [
-        'title',
-        'excerpt',
-        'body',
-        'image_url'
+    protected $guarded = [
+        'id',
+        'created_at',
+        'updated_at',
     ];
 }
