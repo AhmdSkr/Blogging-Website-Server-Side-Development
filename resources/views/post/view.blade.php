ada133868e7a46dfbe4bac0c2453a898e2fcbf4c
Mharks001 <ahmad.mhmd.sakr@gmail.com>
2023-05-12 17:03:35 +0300

    Post view creation and update date added

--- resources/views/post/view.blade.php
+++ resources/views/post/view.blade.php
@@ -1,4 +1,6 @@
 <x-layout.app>
+    <a href="{{route('post.index')}}">< back to index</a>
+    <hr/>
     <img src="{{$post->image_url}}"/>
     <h1>{{$post->title}}</h1>
     <a href="{{route('post.edit', ['post' => $post->id])}}">edit post</a>
@@ -8,4 +10,6 @@
     <h3>Post Content</h3>
     <p>{{$post->body}}</p>
     <hr/>
+    <p><b>Created At:</b> {{$post->created_at}}</p>
+    <p><b>Updated At:</b> {{$post->updated_at}}</p>
 </x-layout.app>
