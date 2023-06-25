f6e6100172f4c64a6610ea78284a6522500d64bd
AhmdSkr <116917027+AhmdSkr@users.noreply.github.com>
2023-06-25 16:53:25 +0300

    KISS applied

--- app/Http/Requests/Post/PostFormRequest.php
+++ app/Http/Requests/Post/PostFormRequest.php
@@ -3,13 +3,12 @@
 namespace App\Http\Requests\Post;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\File;
 
 use App\Models\Post;
-use Illuminate\Support\Facades\File;
 
 abstract class PostFormRequest extends FormRequest
 {
-    private Post|null $instance = null;
 
     /**
      * Determine if the user is authorized to make this request.
@@ -17,47 +16,6 @@ abstract class PostFormRequest extends FormRequest
     public function authorize(): bool
     {
         return true;
-    }
-    
-    /**
-     * Creates the post instance from form request
-     */
-    public abstract function createPostInstance() : Post;
-
-    public function getImageURL($image) : string|null
-    {
-        return null;
-    }
-
-    public function getMinutesToRead() : int 
-    {
-        return 1;
-    }
-
-    public function getValidatedData() : array|null
-    {
-        $attributes = $this->validated();
-        $image = $attributes['image'];
-        unset($attributes['image']);
-        $attributes = array_merge(
-            $attributes, 
-            [
-                'image_url' => $this->getImageURL($image),
-                'minutes_to_read' => $this->getMinutesToRead(),
-            ]
-        );
-        return $attributes;
-    }
-
-    /**
-     * Retrieves the post instance submitted through the form request.
-     */
-     public final function getPostInstance() : Post 
-    {
-        if($this->instance === null) {
-            $this->instance = $this->createPostInstance();
-        }
-        return $this->instance;
     }
 
     /**
