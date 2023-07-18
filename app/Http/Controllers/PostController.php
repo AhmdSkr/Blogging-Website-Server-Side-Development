1bc10afb205a684aa144d37ae8dbe155e950a567
AhmdSkr <116917027+AhmdSkr@users.noreply.github.com>
2023-07-18 15:53:42 +0300

    Post Creation: Added Feature
    Added to post controller actions:
       - create:  display post creation form
       - store: save post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\FileCollectionService;
 use Illuminate\Http\Response;
 
 use App\Models\Post;
@@ -28,4 +29,55 @@ class PostController extends Controller
         return response()->view('post.view', ['post' => $post]);
     }
 
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return response()->view('post.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StorePostRequest $request, FileCollectionService $fileCollect)
+    {
+        $validAttributes = $request->validated();
+        $post = new Post();
+        
+        /* filling post model fields */
+        {
+            $result = null;
+            if(isset($validAttributes['image']) && $validAttributes['image'] !== null)
+            {
+                $result = $fileCollect->upload($validAttributes['image']);
+                if($result === false)
+                {
+                    // Message: failed to uplaod image!
+                    // Continue uploading post, normally.
+                    $result = null;
+                }
+            }
+
+            $post->fill($validAttributes);
+            $post->image_url = $result;
+            $post->minutes_to_read = 1;
+        }
+        
+        if(!$post->save())
+        /* Handle database storage failure here... */
+        {
+            
+            if($post->image_url !== null)
+            {
+                $fileCollect->remove($post->image_url);
+            }
+            abort(Response::HTTP_INTERNAL_SERVER_ERROR,"Unable to upload posted content.");
+        }
+        
+        return redirect(
+            to: route('post.show', ['post' => $post->id]),
+            status: Response::HTTP_CREATED
+        );
+    }
 }
