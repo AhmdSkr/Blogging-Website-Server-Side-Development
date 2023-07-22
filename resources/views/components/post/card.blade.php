3448b50e916757cb4123ed87ce363b30909e56de
AhmdSkr <116917027+AhmdSkr@users.noreply.github.com>
2023-07-22 22:53:50 +0300

    Index view: added "create post" link.

--- resources/views/components/post/card.blade.php
+++ resources/views/components/post/card.blade.php
@@ -2,6 +2,6 @@
 
 <div>
     <img src="{{$post->image_url}}"/>
-    <h3>{{$post->title}}</h3>
+    <a href="{{route("post.show", [ 'post' => $post->id])}}"><h3>{{$post->title}}</h3></a>
     <p>{{$post->excerpt}}</p>
 </div>
--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -1,5 +1,8 @@
 @props(['posts' => []])
 <x-layout.app>
     <h1>All Posts</h1>
+    <a href="{{route("post.index")}}">Create new Post</a>
+    <hr/>
+
     <x-post.collection :$posts/>
 </x-layout.app>
