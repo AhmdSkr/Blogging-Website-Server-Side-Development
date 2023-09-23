94faaf9f449d376e4723154866dfd07ce99caeec
AhmdSkr <116917027+AhmdSkr@users.noreply.github.com>
2023-09-23 15:38:11 +0300

    Post and Blog edit UI

--- resources/views/blog/edit.blade.php
+++ resources/views/blog/edit.blade.php
@@ -1,36 +1,26 @@
 <x-layout.app>
-    @if($errors->any())
-    <ul>
-        @foreach($errors->all() as $error)
-        <li>
-            {{$error}}
-        </li>
-        @endforeach
-    </ul>
-    <hr/>
-    @endif
-    <hr/>
-    
-    @if($blog->image_url !== null)
-    <form action="{{route("blog.uncover", ['blog' => $blog->id])}}" method="POST">
-        @csrf
-        @method("DELETE")
+    {{-- Validation --}}
+    <x-post.validation :errors class="border-b-4"/>
 
-        <img src="{{$blog->image_url}}"/><br/>
-        
-        <input type="submit" value="Remove Cover Image"/>
-
-    </form>
-    @endif
-
-    <hr/>
-    <form action="{{route('blog.update', ['blog' => $blog->id])}}" method="POST" enctype="multipart/form-data">
-        @csrf
-        @method('PATCH')
-        <span><b>Blog Name:</b> </span><input name="name" type="text" value="{{ old('name') ?? $blog->name}}"/><br/>
-        <h3><b>Brief Description:</b> </h3><textarea name="description">  {{old('description') ?? $blog->description}}  </textarea><br/>
-        <h3>Cover Image</h3><input name="image" type="file"/><br/>
-        <hr/>
-        <input type="submit" />
-    </form>
+    <div class="form-main-portal">
+        @if($blog->image_url !== null)
+        <form action="{{route("blog.uncover", ['blog' => $blog->id])}}" method="POST">
+            @csrf
+            @method("DELETE")
+            <img src="{{$blog->image_url}}"/>
+            <input type="submit" value="Remove Cover Image" class="uncover-btn"/>
+        </form>
+        @endif
+        <form action="{{route('blog.update', ['blog' => $blog->id])}}" method="POST" enctype="multipart/form-data">
+            @csrf
+            @method('PATCH')
+            {{-- Name & Cover --}}
+            <div class="form-meta-input">
+                <h3 class="meta-input-label">Blog name</h3><textarea name="name" class="title-input" maxlength="64" required>{{ old('name') ?? $blog->name}}</textarea>
+                <h3 class="meta-input-label">Cover Image</h3><input name="image" type="file" class="image-input"/>
+            </div>
+            <h3>Brief Description:</h3><textarea name="description" class="max-h-52">{{old('description') ?? $blog->description}}</textarea><br/>
+            <input type="submit" class="floating-action-button"/>
+        </form>
+    </div>
 </x-layout.app>
--- resources/views/post/edit.blade.php
+++ resources/views/post/edit.blade.php
@@ -1,37 +1,46 @@
 <x-layout.app>
-    @if($errors->any())
-    <ul>
-        @foreach($errors->all() as $error)
-        <li>
-            {{$error}}
-        </li>
-        @endforeach
-    </ul>
-    <hr/>
-    @endif
-    <hr/>
+    {{-- TinyMCE RichText Editor configuration --}}
+    <x-slot:head>
+        <x-head.tinymce-config/>
+    </x-slot>
+
+    {{-- Validation --}}
+    <x-post.validation :errors class="border-b-4"/>
     
-    @if($post->image_url !== null)
-    <form action="{{route("post.uncover", ['post' => $post->id])}}" method="POST">
-        @csrf
-        @method("DELETE")
-
-        <img src="{{$post->image_url}}"/><br/>
-        
-        <input type="submit" value="Remove Cover Image"/>
-
-    </form>
+    {{-- Review Here --}}
+    @if(isset($target))
+    <div class="flex ">
+        <div class="form-review-portal">
+            <h1>Reviewing Post:</h1><x-post.view :post="$target"/>
+        </div>
     @endif
 
-    <hr/>
-    <form action="{{route('post.update', ['post' => $post->id])}}" method="POST" enctype="multipart/form-data">
-        @csrf
-        @method('PATCH')
-        <span><b>Title:</b> </span><input name="title" type="text" value="{{ old('title') ?? $post->title}}"/><br/>
-        <h3><b>Excerpt:</b> </h3><textarea name="excerpt">  {{old('excerpt') ?? $post->excerpt}}  </textarea><br/>
-        <h3>Post Content</h3><textarea name="body">         {{old('body') ?? $post->body}}     </textarea><br/>
-        <h3>Cover Image</h3><input name="image" type="file"/><br/>
-        <hr/>
-        <input type="submit" />
-    </form>
+    <div class="form-main-portal">
+        {{-- Uncover Form --}}
+        @if($post->image_url !== null)
+        <form action="{{route("post.uncover", ['post' => $post->id])}}" method="POST"> @csrf @method("DELETE")
+            <img src="{{$post->image_url}}"/>
+            <input type="submit" value="Remove Cover Image" class="btn btn-square btn-error btn-block rounded-none"/>
+        </form>
+        @endif
+
+        {{-- Editing Form --}}
+        <form action="{{route('post.update', ['post' => $post->id])}}" method="POST" enctype="multipart/form-data">
+            @csrf
+            @method('PATCH')
+            {{-- Title & Cover --}}
+            <div class="form-meta-input">
+                <h3 class="meta-input-label">Title</h3><textarea name="title" class="title-input" maxlength="64" required>{{ old('title') ?? $post->title}}</textarea>
+                <h3 class="meta-input-label">Cover Image</h3><input name="image" type="file" class="image-input"/>
+            </div>
+            {{-- Excerpt & Body --}}
+            <div class="form-main-input">
+                {{-- Discard button --}}<a href="{{route('post.index')}}" class="btn btn-error">Discard</a>
+                {{-- Submit button --}}<input type="submit" class="btn btn-success"/>
+                {{-- Excerpt --}}<h3>Excerpt</h3><textarea name="excerpt" class="max-h-72" maxlength="256">{{old('excerpt') ?? $post->excerpt}}</textarea><br/>
+                {{-- Post Content --}}<h3>Post Content</h3><textarea name="body" class="h-96" id="tinyeditor"> {{old('body') ?? $post->body}}</textarea>
+            </div>
+        </form>
+    </div>
+
 </x-layout.app>
